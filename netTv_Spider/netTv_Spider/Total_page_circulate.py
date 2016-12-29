76dbe8b6db137900f97494a86e4dc01ddda44dc7
tingyun <13284638588@163.com>
2016-12-29 18:25:18 +0800

    update tudou_show crawler.

--- netTv_Spider/netTv_Spider/Total_page_circulate.py
+++ netTv_Spider/netTv_Spider/Total_page_circulate.py
@@ -3,16 +3,8 @@
 #这个文件：分离出所有需要计算站点实际page页面的木块，一个可扩展的if elif语句来实现
 
 def Total_page_circulate(site_name,max_pages):
-	if site_name == "xiami_music":
-			return (max_pages/24)+1
-	if site_name == "xiami_album":
-			return (max_pages/24)+1
-	if site_name == "xiami_mv":
-			return (max_pages/24)+1
-	if site_name == "letv":
-			return (max_pages/30)+1
-	if site_name == "tudou":
-			return (max_pages/40)+1
+	if site_name == "tudou_show":
+			return (max_pages/90)+1
 	else:
 			return max_pages
 
@@ -26,8 +18,8 @@ def Turn_True_Page(i,site_name):
 
 
 def Total_page_circulate2(site_name,max_pages):
-	if site_name == "letv":
-			return (max_pages/14)+1
+	if site_name == "tudou_show":
+			return (max_pages/90)+1
 	else:
 			return max_pages
 
