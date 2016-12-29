f625bf2ea250a6fa1d5d0177865b76a0a1cb3820
tingyun <13284638588@163.com>
2016-12-29 11:25:35 +0800

    fix the crawler youku_show's bug.

--- netTv_Spider/netTv_Spider/spiders/YoukuShow_Spider.py
+++ netTv_Spider/netTv_Spider/spiders/YoukuShow_Spider.py
@@ -29,7 +29,7 @@ from netTv_Spider.Total_page_circulate import Total_page_circulate,Turn_True_Pag
 
 
 class netTvSpider(scrapy.Spider):
-	name ='2youku_show'
+	name ='youku_show'
 	allowed_domain = []
 		
 	def __init__(self,*args,**kwargs):
@@ -128,7 +128,7 @@ class netTvSpider(scrapy.Spider):
 								yield request
 				else:
 						for url in self.Index_Url:
-								request = Request(url,callback = self.parse_splash,meta={
+								request = Request(url,callback = self.parse_splash,dont_filter=True,meta={
 											'splash':{
 													'endpoint':'render.html',
 													'args':{
@@ -210,7 +210,6 @@ class netTvSpider(scrapy.Spider):
 				for i in range(1,2):
 						i = Turn_True_Page(i,self.name)
 						url = urls.format(page=str(i))
-						print "now the url is %s"%url
 						request = Request(url,callback = self.parse_final,dont_filter=True,meta={
 											'splash':{
 											'endpoint':'render.html',
@@ -227,14 +226,15 @@ class netTvSpider(scrapy.Spider):
 				#for i in range(1,int(max_pages)+1):
 				for i in range(1,2):
 						try:
+								i = Turn_True_Page(i,self.name)
 								url = urls.format(page=str(i))
 						except Exception,e:
 								print Exception,":",e
-						request = Request(url,callback = self.parse_first,meta={
+						request = Request(url,callback = self.parse_first,dont_filter=True,meta={
 										'splash':{
 										'endpoint':'render.html',
 										'args':{
-												'wait':5,
+												'wait':0.5,
 												'images':0,
 												'render_all':1
 												}
@@ -366,14 +366,15 @@ class netTvSpider(scrapy.Spider):
 				for url in Relative_to_Absolute(Index_Url,response.xpath(xpath).extract()):
 						detail_url.append(url)
 		#在考虑在每一层加一个判断，相当于如果没有（第一个）要传递给下一层的数据，就直接传递给final_parse（注：在传递给final_parse时需要判断是否需要渲染，这里我暂时先默认都渲染，但是之后可以考虑在config.json的Final_Xpath加一个flag，1表示需要渲染，0表示不需要）
+		print "detail url are %s \n"%detail_url
 		if Signal_Detail_Page is None:
 				for url in detail_url:
-						request = Request(url,callback = self.parse_final,meta={
+						request = Request(url,callback = self.parse_final,dont_filter=True,meta={
 											'splash':{
 											'endpoint':'render.html',
 											'args':{
-													#只有aiyiyi需要load 8s，才能拿到播放量
-													'wait':10,
+													#只有aiyiyi需要load 10s，才能拿到播放量
+													'wait':0.5,
 													'images':0,
 													'render_all':1
 													}
@@ -384,7 +385,17 @@ class netTvSpider(scrapy.Spider):
 						yield request
 		else:
 				for url in detail_url:
-						request = Request(url,callback = self.parse_second)
+						#因为特殊情况，这个页面都需要渲染，保证拿到下一页的link
+						request = Request(url,callback = self.parse_second,dont_filter=True,meta={
+											'splash':{
+											'endpoint':'render.html',
+											'args':{
+													'wait':0.5,
+													'images':0,
+													'render_all':1
+													}
+											}
+									})
 						request.meta['Index_Url'] = Index_Url
 						request.meta['Signal_Detail_Page'] = Signal_Detail_Page
 						request.meta['Target_Detail_Page'] = Target_Detail_Page
@@ -404,10 +415,12 @@ class netTvSpider(scrapy.Spider):
 								Some_Info[key] = response.xpath(Signal_Detail_Page['Some_Info'][key]).extract()[0]
 						except Exception,e:
 								print Exception,":",e
+		print "fuck，这个signal_detail_page is %s \n"%Signal_Detail_Page['xpath']
+		print response.xpath(Signal_Detail_Page['xpath']).extract()
 		detail_url = Relative_to_Absolute(Index_Url,response.xpath(Signal_Detail_Page['xpath']).extract())
 		if Target_Detail_Page is None:
 				for url in detail_url:
-						request = Request(url,callback = self.parse_final,meta={
+						request = Request(url,callback = self.parse_final,dont_filter=True,meta={
 												'splash':{
 												'endpoint':'render.html',
 												'args':{
@@ -423,7 +436,7 @@ class netTvSpider(scrapy.Spider):
 		else:
 				for url in detail_url:
 						#print "now the url is %s"%url
-						request = Request(url,callback = self.parse_third)
+						request = Request(url,callback = self.parse_third,dont_filter=True)
 						request.meta['Index_Url'] = Index_Url
 						request.meta['Target_Detail_Page'] = Target_Detail_Page
 						request.meta['Final_Xpath'] = Final_Xpath
